36d9fd194d472be4502aacae82ade79bb1526ea6
Bartosz Fórmanowski <bartosz.f@masterborn.com>
2025-04-28 22:47:51 +0200

    refactor: move form component out of ui

--- src/components/form.tsx
+++ src/components/form.tsx
@@ -12,7 +12,7 @@ import { createFormHook, createFormHookContexts } from "@tanstack/react-form";
 export const { fieldContext, formContext, useFieldContext } =
   createFormHookContexts();
 
-export const { useAppForm } = createFormHook({
+export const { useAppForm, withForm } = createFormHook({
   fieldContext,
   formContext,
   fieldComponents: {
